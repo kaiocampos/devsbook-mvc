ff9214711d44a119466f17936ac4eb093f0cca2e
Kaio Campos <kaiocamposti@gmail.com>
2020-06-06 08:37:24 -0300

    Implementando o construtor para verificar se tem usuario na session

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,12 +1,27 @@
 <?php
+
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
+    private $loggedUser;
 
-    public function index() {
+    public function __construct()
+    {
+        $this->loggedUser = LoginHandler::checkLogin();
+
+        if ( $this->loggedUser === false) {
+            $this->redirect('/login');
+        }
+        
+    }
+
+    public function index()
+    {
+        
         $this->render('home', ['nome' => 'Bonieky']);
     }
-   
 }
