a47f2e62d3d1eb7cb76590207779aec09ad9e532
Kaio Campos <kaiocamposti@gmail.com>
2020-06-10 08:06:12 -0300

    implementando a functin gethomefeed

--- src/handlers/PostHandler.php
+++ src/handlers/PostHandler.php
@@ -2,6 +2,8 @@
 namespace src\handlers;
 
 use \src\models\Post;
+use \src\models\User;
+use \src\models\UserRelation;
 
 class PostHandler{
     public static function addPost($idUser, $type, $body){
@@ -15,5 +17,23 @@ class PostHandler{
                 'body' => $body
             ])->execute();
         }
+    }
+
+    public static function getHomeFeed($idUser){
+        // 1. pegar lista de usuários que EU sigo.
+        $userList = UserRelation::select()->where('user_from', $idUser)->get();
+        $users = [];
+        foreach($userList as $userItem){
+            $users[] = $userItem['user_to'];
+
+        }
+        $users[] = $idUser;
+        print_r($users);
+
+        // 2.  pegar os posts dessa galera ordenado pela data.
+        // 3. transformar o resultado em objetos dos models
+        // 4. preencher as informçaões adicionais no post
+        // 5. retornar o resultad.
+
     }
 }
