5362ebc662c6ac02d32c4563e38ce9dc08c05744
Kaio Campos <kaiocamposti@gmail.com>
2020-06-11 11:24:22 -0300

    Recebendo e exibindo dados dos posts e usuários

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,41 +1,46 @@
-<?= $render('header',['loggedUser' => $loggedUser]); ?>
+<?= $render('header', ['loggedUser' => $loggedUser]); ?>
 
 <section class="container main">
-<?= $render('sidebar'); ?>
-<section class="feed mt-10">
+    <?= $render('sidebar'); ?>
+    <section class="feed mt-10">
 
-<div class="row">
-<div class="column pr-5">
-
-    
-<?=$render('feed-editor', ['user'=> $loggedUser]);?>
-<?=$render('feed-item');?>
+        <div class="row">
+            <div class="column pr-5">
 
 
+                <?= $render('feed-editor', ['user' => $loggedUser]); ?>
+
+                <?php foreach($feed as $feedItem):?>
+                    <?= $render('feed-item',
+                        ['data' => $feedItem,
+                        'loggedUser'=> $loggedUser
+                        ]); ?>
+                <?php endforeach;?>
 
 
-</div>
-<div class="column side pl-5">
-    <div class="box banners">
-        <div class="box-header">
-            <div class="box-header-text">Patrocinios</div>
-            <div class="box-header-buttons">
 
             </div>
-        </div>
-        <div class="box-body">
-            <a href=""><img src="https://alunos.b7web.com.br/media/courses/logo/php.jpg" /></a>
-            <a href=""><img src="https://alunos.b7web.com.br/media/courses/logo/laravel.jpg" /></a>
-        </div>
-    </div>
-    <div class="box">
-        <div class="box-body m-10">
-            Criado com ❤️ por B7Web
-        </div>
-    </div>
-</div>
-</div>
+            <div class="column side pl-5">
+                <div class="box banners">
+                    <div class="box-header">
+                        <div class="box-header-text">Patrocinios</div>
+                        <div class="box-header-buttons">
 
+                        </div>
+                    </div>
+                    <div class="box-body">
+                        <a href=""><img src="https://alunos.b7web.com.br/media/courses/logo/php.jpg" /></a>
+                        <a href=""><img src="https://alunos.b7web.com.br/media/courses/logo/laravel.jpg" /></a>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="box-body m-10">
+                        Criado com ❤️ por B7Web
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
 </section>
-</section>
-<?=$render('footer');?>
+<?= $render('footer'); ?>
