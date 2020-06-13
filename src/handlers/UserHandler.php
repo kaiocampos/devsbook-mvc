1e8524bb8dcebc32e11928ce6b14e3085d3c45d4
Kaio Campos <kaiocamposti@gmail.com>
2020-06-13 15:00:02 -0300

    Implementando o getUser com todas a suas funcionalidades

--- src/handlers/UserHandler.php
+++ src/handlers/UserHandler.php
@@ -3,6 +3,7 @@
 namespace src\handlers;
 
 use \src\models\User;
+use \src\models\UserRelation;
 
 class UserHandler
 {
@@ -53,6 +54,64 @@ class UserHandler
         return $user ? true : false;
 
     }
+
+    public static function getUser($id, $full = false){
+        $data = User::select()->where('id', $id)->one();
+
+        if ($data) {
+            $user = new User();
+            $user->id = $data['id'];
+            $user->name = $data['name'];
+            $user->birthdate = $data['birthdate'];
+            $user->city = $data['city'];
+            $user->work = $data['work'];
+            $user->avatar = $data['avatar'];
+            $user->cover = $data['cover'];
+
+            if($full){
+                $user->followers = [];
+                $user->following = [];
+                $user->photos = [];
+
+                //followers
+                $followers = UserRelation::select()->where('user_to', $id)->get();
+                foreach($followers as $follower){
+                    $userData = User::select()->where('id', $follower['user_from'])->one();
+                    
+                    $newUser = new User();
+                    $newUser->id = $userData['id'];  
+                    $newUser->name = $userData['name'];
+                    $newUser->avatar = $userData['avatar'];
+
+                    $user->followeres[] = $newUser;
+
+                }
+
+                //following
+                $following = UserRelation::select()->where('user_from', $id)->get();
+                foreach($following as $follower){
+                    $userData = User::select()->where('id', $follower['user_to'])->one();
+                    
+                    $newUser = new User();
+                    $newUser->id = $userData['id'];  
+                    $newUser->name = $userData['name'];
+                    $newUser->avatar = $userData['avatar'];
+
+                    $user->following[] = $newUser;
+
+                }
+
+                //photos
+
+            }
+
+            return $user;
+        }
+
+        return false;
+
+    }
+
     public static function addUser($name, $email, $password, $birthdate){
         $hash = password_hash($password, PASSWORD_DEFAULT);
         $token = md5(time().rand(0,9999).time());
